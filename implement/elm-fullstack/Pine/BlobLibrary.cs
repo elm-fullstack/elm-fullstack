c1584da15e8ffe416d20cae43a497a0f89843697
Michael Rätzel <viir@viir.de>
2021-05-27 11:02:42 +0000

    Test theory about runtime expenses

--- implement/elm-fullstack/Pine/BlobLibrary.cs
+++ implement/elm-fullstack/Pine/BlobLibrary.cs
@@ -23,8 +23,8 @@ namespace Pine
 
                 var blobCandidate = getter(sha256);
 
-                if (!(Enumerable.SequenceEqual(Composition.GetHash(Composition.Component.Blob(blobCandidate)), sha256) ||
-                    Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256)))
+                if (!(Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256)) ||
+                    Enumerable.SequenceEqual(Composition.GetHash(Composition.Component.Blob(blobCandidate)), sha256))
                     return null;
 
                 return blobCandidate;
@@ -46,8 +46,8 @@ namespace Pine
             var cacheFilePath = Path.Combine(cacheDirectory, sha256DirectoryName, fileName);
 
             bool blobHasExpectedSHA256(byte[] blobCandidate) =>
-                Enumerable.SequenceEqual(Composition.GetHash(Composition.Component.Blob(blobCandidate)), sha256) ||
-                Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256);
+                Enumerable.SequenceEqual(CommonConversion.HashSHA256(blobCandidate), sha256) ||
+                Enumerable.SequenceEqual(Composition.GetHash(Composition.Component.Blob(blobCandidate)), sha256);
 
             try
             {
